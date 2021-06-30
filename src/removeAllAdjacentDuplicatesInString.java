7e4989140579e8509b9010a0b0bc5e6812b66d86
micos7 <micos7@gmail.com>
2021-06-30 11:21:08 +0300

    running-sum-of-1d-array

---
+++ src/removeAllAdjacentDuplicatesInString.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class removeAllAdjacentDuplicatesInString {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack();
+        for (char c : s.toCharArray()) {
+            if (st.size() > 0 && st.peek() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (st.size() > 0) {
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
