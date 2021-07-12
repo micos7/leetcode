1b524d7f26b0d0fa03bca231eb1b614cba02442b
micos7 <micos7@gmail.com>
2021-07-12 18:17:13 +0300

    Longest Increasing Subsequence O(n^2)

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -4,16 +4,34 @@ public class LongestIncreasingSubsequence {
     //https://leetcode.com/problems/longest-increasing-subsequence/ 300
     public int lengthOfLIS(int[] nums) {
         TreeSet<Integer> ts = new TreeSet<>();
-        for(int num: nums){
+        for (int num : nums) {
             Integer highest = ts.ceiling(num);
-            if(highest == null){
+            if (highest == null) {
                 ts.add(num);
-            }else{
+            } else {
                 ts.remove(highest);
                 ts.add(num);
             }
         }
 
         return ts.size();
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int max = 1;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
+            }
+        }
+        return max;
     }
 }
