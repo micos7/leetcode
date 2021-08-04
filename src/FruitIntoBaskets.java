60e9c35195692954d9b3b5a786e4df005ca2aa47
micos7 <micos7@gmail.com>
2021-08-04 18:43:49 +0300

    Fruit Into Baskets - simpler

--- src/FruitIntoBaskets.java
+++ src/FruitIntoBaskets.java
@@ -28,4 +28,36 @@ public class FruitIntoBaskets {
 
         return max;
     }
+
+    public int totalFruit(int[] fruits) {
+        int last_fruit = -1;
+        int second_last_fruit = -1;
+        int last_fruit_count = 0;
+        int current_max= 0;
+        int max = 0;
+
+        for(int fruit : fruits){
+            if(fruit == last_fruit || fruit == second_last_fruit){
+                current_max++;
+            }else{
+                current_max = last_fruit_count + 1;
+            }
+
+            if(fruit == last_fruit){
+                last_fruit_count++;
+            }else{
+                last_fruit_count = 1;
+            }
+
+            if(fruit != last_fruit){
+                second_last_fruit = last_fruit;
+                last_fruit = fruit;
+            }
+
+            max = Math.max(max,current_max);
+        }
+
+        return max;
+
+    }
 }
