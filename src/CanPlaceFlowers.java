06d3075e7e56af7601a587a4a6e14fd4d8cc17a5
micos7 <micos7@gmail.com>
2021-08-06 07:38:00 +0300

    Can Place Flowers

---
+++ src/CanPlaceFlowers.java
@@ -0,0 +1,24 @@
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //https://leetcode.com/problems/can-place-flowers/ 605
+        int flowers = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
+                int next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
+
+
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    flowers++;
+                }
+            }
+
+
+        }
+
+        return flowers >= n;
+
+    }
+}
