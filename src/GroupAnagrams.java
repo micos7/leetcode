d78cc0e4bf34864e505e7915013f2836e86425f9
micos7 <micos7@gmail.com>
2021-08-18 11:00:21 +0300

    Group Anagrams

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //https://leetcode.com/problems/group-anagrams/ 49
+        List<List<String>> ans = new ArrayList<>();
+        HashMap<String,List<String>> hm = new HashMap<>();
+        for(String s:strs){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if(!hm.containsKey(sorted)){
+                hm.put(sorted,new ArrayList<>());
+            }
+            hm.get(sorted).add(s);
+        }
+
+        ans.addAll(hm.values());
+
+        return ans;
+    }
+}
