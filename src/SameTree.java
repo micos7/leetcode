e026552bc17ce24f3a41e48eda65b72f018e6a5a
micos7 <micos7@gmail.com>
2021-09-01 12:11:19 +0300

    Same Tree

---
+++ src/SameTree.java
@@ -0,0 +1,14 @@
+public class SameTree {
+    //https://leetcode.com/problems/same-tree/ 100
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
