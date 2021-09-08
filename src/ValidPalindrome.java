325191c76e1f11067208bd61c84af01240df75ba
micos7 <micos7@gmail.com>
2021-09-08 10:00:47 +0300

    Valid palindrome

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,20 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        //https://leetcode.com/problems/valid-palindrome/ 125
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (left < right && Character.toLowerCase(s.charAt(left++)) != Character.toLowerCase(s.charAt(right--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
