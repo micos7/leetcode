9d5512e3c0118fb59dbe7acf62395d15564b4ab8
micos7 <micos7@gmail.com>
2021-11-08 09:21:55 +0200

    Flip Equivalent Binary Trees

---
+++ src/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,12 @@
+public class FlipEquivalentBinaryTrees {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        //https://leetcode.com/problems/flip-equivalent-binary-trees/ 951
+        if(root1 ==null && root2 == null) return true;
+        if(root1 ==null || root2 == null || root1.val != root2.val) return false;
+
+        boolean symetrical = flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right);
+        boolean asymetrical = flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left);
+
+        return symetrical || asymetrical;
+    }
+}
