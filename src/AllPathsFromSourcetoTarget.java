16a1147ae7e53a0024e98956457dbb962e8c4d33
micos7 <micos7@gmail.com>
2021-11-29 17:01:31 +0200

    All Paths From Source to Target dfs

--- src/AllPathsFromSourcetoTarget.java
+++ src/AllPathsFromSourcetoTarget.java
@@ -24,4 +24,26 @@ public class AllPathsFromSourcetoTarget {
         }
         return ans;
     }
+
+    //dfs approach
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> t = new ArrayList<>();
+        t.add(0);
+        helper(graph,0,t,ans);
+        return ans;
+    }
+
+    private void helper(int[][] graph,int position,List<Integer> temp,List<List<Integer>> ans){
+        if(position == graph.length-1){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int neighbour:graph[position]){
+            temp.add(neighbour);
+            helper(graph,neighbour,temp,ans);
+            temp.remove(temp.size()-1);
+        }
+
+    }
 }
