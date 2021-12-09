1a0ce8f3295e1802d97d8eec480728e795cf8ee1
micos7 <micos7@gmail.com>
2021-12-09 10:18:20 +0200

    memoization

--- src/TargetSum.java
+++ src/TargetSum.java
@@ -1,14 +1,24 @@
+import java.util.HashMap;
+
 public class TargetSum {
     public int findTargetSumWays(int[] nums, int target) {
-        //https://leetcode.com/problems/target-sum/ 494
-        return helper(nums, 0, 0, target);
+        //https://leetcode.com/problems/target-sum/submissions/ 494
+        HashMap<String,Integer> hm = new HashMap<>();
+        return helper(nums,0,0,target,hm);
     }
 
-    private int helper(int[] nums, int pos, int sum, int target) {
-        if (nums.length == pos) {
+    private int helper(int[] nums,int pos,int sum,int target,HashMap<String,Integer> hm){
+        if(nums.length == pos){
             return sum == target ? 1 : 0;
         }
-        return helper(nums, pos + 1, sum - nums[pos], target) +
-                helper(nums, pos + 1, sum + nums[pos], target);
+        String key = pos + " " + sum;
+        if(hm.containsKey(key)){
+            return hm.get(key);
+        }
+        int val =  helper(nums,pos+1,sum -nums[pos],target,hm)+
+                helper(nums,pos+1,sum + nums[pos],target,hm);
+
+        hm.put(key,val);
+        return val;
     }
 }
