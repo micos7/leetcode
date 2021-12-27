5670247684ab5d9d643bf2683337e323f5a484e0
micos7 <micos7@gmail.com>
2021-12-27 14:50:19 +0200

    priority queue

--- src/AdvantageShuffle.java
+++ src/AdvantageShuffle.java
@@ -23,4 +23,33 @@ public class AdvantageShuffle {
 
         return ans;
     }
+    //priority queue
+    public int[] advantageCount(int[] nums1, int[] nums2) {
+
+        int[] ans = new int[nums1.length];
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->(b[1] - a[1]));
+        for(int i=0;i<nums1.length;i++){
+            pq.offer(new int[]{i,nums2[i]});
+        }
+
+        int low = 0;
+        int high = nums1.length-1;
+        Arrays.sort(nums1);
+
+        while(!pq.isEmpty()){
+            int[] head =pq.poll();
+            int max = head[1];
+            int index = head[0];
+
+            if(nums1[high] > max){
+                ans[index] = nums1[high];
+                high--;
+            }else{
+                ans[index] = nums1[low];
+                low++;
+            }
+        }
+
+        return ans;
+    }
 }
