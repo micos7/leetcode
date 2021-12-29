0551c95f89be20f6bf75c6a57c425246002661c1
micos7 <micos7@gmail.com>
2021-12-29 11:38:29 +0200

    Remove Duplicate Letters

---
+++ src/RemoveDuplicateLetters.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class RemoveDuplicateLetters {
+    public String removeDuplicateLetters(String s) {
+        //https://leetcode.com/problems/remove-duplicate-letters/ 316
+        int[] lastIndex = new int[26];
+        for(int i = 0;i<s.length();i++){
+            lastIndex[s.charAt(i) -'a'] = i;
+        }
+        boolean[] seen = new boolean[26];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            int c = s.charAt(i) - 'a';
+            if(seen[c]){
+                continue;
+            }
+            while(!st.isEmpty() && st.peek() > c && i< lastIndex[st.peek()] ){
+                seen[st.pop()] = false;
+            }
+            st.push(c);
+            seen[c] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append((char) (st.pop() + 'a'));
+        }
+
+        return sb.reverse().toString();
+
+    }
+}
