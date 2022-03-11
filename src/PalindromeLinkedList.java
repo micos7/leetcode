690d5348dc084c22e7391eead6fd2027d2923e7a
micos7 <micos7@gmail.com>
2022-03-11 11:04:49 +0200

    Palindrome Linked List

---
+++ src/PalindromeLinkedList.java
@@ -0,0 +1,33 @@
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        //https://leetcode.com/problems/palindrome-linked-list/ 234
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
