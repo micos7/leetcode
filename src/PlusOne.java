45dc6f6d9256fead99ba73e0dfded1166f178737
micos7 <micos7@gmail.com>
2022-03-28 09:36:37 +0300

    Plus One

---
+++ src/PlusOne.java
@@ -0,0 +1,17 @@
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        //https://leetcode.com/problems/plus-one/ 66
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+
+    }
+}
