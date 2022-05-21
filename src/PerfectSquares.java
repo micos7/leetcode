2a40da19e16a9953cc78345e32c7d57beafd84b6
micos7 <micos7@gmail.com>
2022-05-21 12:01:09 +0300

    Perfect Squares

---
+++ src/PerfectSquares.java
@@ -0,0 +1,13 @@
+public class PerfectSquares {
+    public int numSquares(int n) {
+        //https://leetcode.com/problems/perfect-squares/ 279
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
