fea9dd4d9eb675d6f0b0b1124674788ae7892dc9
micos7 <micos7@gmail.com>
2022-07-22 21:31:45 +0300

    Relative Sort Array

---
+++ src/RelativeSortArray.java
@@ -0,0 +1,19 @@
+public class RelativeSortArray {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        //https://leetcode.com/problems/relative-sort-array/ 1122
+        int[] count = new int[1001];
+        for (int n : arr1) count[n]++;
+        int i = 0;
+        for (int n : arr2) {
+            while (count[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        for (int n = 0; n < count.length; n++) {
+            while (count[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        return arr1;
+    }
+}
