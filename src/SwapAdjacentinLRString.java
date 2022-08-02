b3d4919a9bcc086a462f39b38ec40afd692dfc37
micos7 <micos7@gmail.com>
2022-08-02 21:04:58 +0300

    Swap Adjacent in LR String

---
+++ src/SwapAdjacentinLRString.java
@@ -0,0 +1,20 @@
+public class SwapAdjacentinLRString {
+    public boolean canTransform(String start, String end) {
+        //https://leetcode.com/problems/swap-adjacent-in-lr-string/ 777
+        if (!start.replace("X", "").equals(end.replace("X", ""))) return false;
+
+        int i = 0, j = 0;
+        int len = start.length();
+
+        while (i < len && j < len) {
+            while (i < len && start.charAt(i) == 'X') i++;
+            while (j < len && end.charAt(j) == 'X') j++;
+            if (i < len && start.charAt(i) == 'L' && i < j) return false;
+            if (i < len && start.charAt(i) == 'R' && i > j) return false;
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+}
